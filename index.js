e5e4ba7c44d70dc7df798bd349eab47b576eec83
amkurian <amkurian@gmail.com>
2019-01-14 12:58:59 +0530

    Parsing payloads implemented

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 //dependencies
 var http = require('http');
 var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder
 
 
 //server should respond to request with a string
@@ -13,10 +14,26 @@ var server = http.createServer(function(req, res){
 	//Get the path
 	var path = parsedUrl.pathname
 	var trimmedPath = path.replace(/^\/+|\/+$/g, '');
-	//Send the response
-  res.end('test');
-	//log the request path
-	console.log(`The Request is received on ${trimmedPath}`)
+  //Get Query string as an object
+  var queryStringObject = parsedUrl.query;
+	//Get http method
+	var method = req.method.toLowerCase()
+	//Get headers as an object
+	var headers =  req.headers;
+	//Get the payloads
+	var decoder = new StringDecoder('utf-8');
+	var buffer =''
+	req.on('data', function(data){
+    buffer += decoder.write(data);
+	})
+
+	req.on('end', function(){
+    buffer += decoder.end();
+    //Send the response
+	  res.end('hello');
+		//log the request path
+		console.log(`The Request is received on ${trimmedPath}  ${buffer}` );
+	})
 });
 
 server.listen(3000, function(){
